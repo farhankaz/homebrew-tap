93ab8e5459beda8c3750d92348d40703637d0619
Farhan Kazmi <farhan.kazmi@gmail.com>
2025-03-16 20:14:29 -0700

    Updated python resource dependencies

--- Formula/snapsense.rb
+++ Formula/snapsense.rb
@@ -17,12 +17,12 @@ class Snapsense < Formula
   end
   
   resource "watchdog" do
-    url "https://files.pythonhosted.org/packages/95/a6/d6ef450ed3f7a5b5f9e2a41a9cc051f6d594243d816ca6a815e6fca3bf29/watchdog-3.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/95/a6/d6ef450393dac5734c63c40a131f66808d2e6f59f6165ab38c98fbe4e6ec/watchdog-3.0.0.tar.gz"
     sha256 "4d98a320595da7a7c5a18fc48cb633c2e73cda78f93cac2ef42d42bf609a33f9"
   end
   
   resource "configparser" do
-    url "https://files.pythonhosted.org/packages/0b/65/bad3eb64f30657ee9fa2e00e80b3ad42037db5eb534fadd15a94a11fe979/configparser-5.3.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/4b/c0/3a47084aca7a940ed1334f89ed2e67bcb42168c4f40c486e267fe71e7aa0/configparser-5.3.0.tar.gz"
     sha256 "8be267824b541c09b08db124917f48ab525a6c3e837011f3130781a224c57090"
   end
 
@@ -31,14 +31,14 @@ class Snapsense < Formula
     venv = virtualenv_create(libexec, "python3.12")
     venv.pip_install resources
     
-    # Install the package itself
-    venv.pip_install_and_link buildpath
+    # Install the Python files manually
+    libexec.install Dir["*.py"]
     
     # Create a wrapper script for snapsense
     (bin/"snapsense").write <<~EOS
       #!/bin/bash
-      export PYTHONPATH="#{libexec}/lib/python3.12/site-packages:$PYTHONPATH"
-      exec "#{libexec}/bin/python" "#{libexec}/bin/snapsense_cli.py" "$@"
+      export PYTHONPATH="#{libexec}:$PYTHONPATH"
+      exec "#{libexec}/bin/python" "#{libexec}/snapsense_cli.py" "$@"
     EOS
     
     # Make the wrapper script executable
@@ -65,12 +65,24 @@ class Snapsense < Formula
 
   def post_install
     # Create a symlink from the user's config directory to the Homebrew config
-    system "mkdir", "-p", "#{ENV["HOME"]}/.config/snapsense"
-    system "ln", "-sf", "#{etc}/snapsense/config.ini", "#{ENV["HOME"]}/.config/snapsense/config.ini"
+    begin
+      system "mkdir", "-p", "#{ENV["HOME"]}/.config/snapsense"
+      system "ln", "-sf", "#{etc}/snapsense/config.ini", "#{ENV["HOME"]}/.config/snapsense/config.ini"
+    rescue
+      opoo "Could not create symlink for config file. You may need to manually create it:"
+      opoo "  mkdir -p #{ENV["HOME"]}/.config/snapsense"
+      opoo "  ln -sf #{etc}/snapsense/config.ini #{ENV["HOME"]}/.config/snapsense/config.ini"
+    end
     
     # Create a symlink from the user's log directory to the Homebrew log
-    system "mkdir", "-p", "#{ENV["HOME"]}/Library/Logs"
-    system "ln", "-sf", "#{var}/log/snapsense/snapsense.log", "#{ENV["HOME"]}/Library/Logs/snapsense.log"
+    begin
+      system "mkdir", "-p", "#{ENV["HOME"]}/Library/Logs"
+      system "ln", "-sf", "#{var}/log/snapsense/snapsense.log", "#{ENV["HOME"]}/Library/Logs/snapsense.log"
+    rescue
+      opoo "Could not create symlink for log file. You may need to manually create it:"
+      opoo "  mkdir -p #{ENV["HOME"]}/Library/Logs"
+      opoo "  ln -sf #{var}/log/snapsense/snapsense.log #{ENV["HOME"]}/Library/Logs/snapsense.log"
+    end
   end
 
   def caveats
